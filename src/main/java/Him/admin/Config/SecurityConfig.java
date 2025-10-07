3a33b557f82a12c4551ef1e0ac79dc0a5c114c98
nyakondaa <anesu.nyakonda@nbs.co.zw>
2025-10-07 13:14:31 +0200

    frontend cors

--- src/main/java/Him/admin/Config/SecurityConfig.java
+++ src/main/java/Him/admin/Config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://him-finance-system-final.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         config.setExposedHeaders(List.of("Authorization", "Content-Type"));
